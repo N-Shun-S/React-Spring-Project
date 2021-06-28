c809b3fbc8d875eea80b6d7f3d18f2b65f0253f9
shun nagakura <shun.nagakura@grandchallenge.co.jp>
2021-06-28 21:17:33 +0900

    fix const path

--- src/Constants.js
+++ src/Constants.js
@@ -1,4 +1,6 @@
-export const API_URL = 'http://localhost:8080'
+//export const API_URL = 'http://localhost:8080'
+export const API_URL = 'http://192.168.56.100:8080'
 
 // jpa確認
-export const JPA_API_URL = 'http://localhost:8080/jpa'
+//export const JPA_API_URL = 'http://localhost:8080/jpa'
+export const JPA_API_URL = 'http://192.168.56.100:8080/jpa'
